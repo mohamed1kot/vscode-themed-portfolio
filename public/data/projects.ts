7b33d2483e30f3d5dcde5662758f47e4c2c35053
mohamed <mohamedamgedkhotaby@gmail.com>
2025-07-04 01:39:40 +0300

    add some of changes

--- public/data/projects.ts
+++ public/data/projects.ts
@@ -23,7 +23,7 @@ export const projects = [
     Source: "https://github.com/mohamed1kot/FordGoBike-data-pipeline", // Optional!
   },
   {
-    ImageURL: "/assets/churn_rate_contract.png",
+    ImageURL: "/assets/churn_rate_contract.gif",
     Title: "📊 Customer Churn Analysis Dashboard.",
     Description: "This repository contains a comprehensive Customer Churn Analysis Dashboard developed using Power BI, focused on analyzing customer attrition trends across multiple dimensions and timeframes. The dashboard enables stakeholders to identify critical churn drivers, segment customers dynamically, and derive strategic insights to improve customer retention.",
     Technologies: [
@@ -39,7 +39,7 @@ export const projects = [
     Source: "https://github.com/mohamed1kot/ChurnAnalysisDashboard", // Optional!
   },
   {
-    ImageURL: "/assets/Overview (1).png",
+    ImageURL: "/assets/Overview (1).gif",
     Title: "Plant Co. Performance Dashboard (Power BI)",
     Description: "This Power BI project provides a comprehensive analysis of the performance of Plant Co., focusing on key business metrics such as Gross Profit, Sales, Quantity, and COGS. The project includes both an overall summary and a Year-To-Date (YTD) vs. Prior Year-To-Date (PYTD) comparison for 2024.",
     Technologies: [
@@ -54,7 +54,7 @@ export const projects = [
     Source: "https://github.com/mohamed1kot/Plant_Co._Performance_Dashboard", // Optional!
   },
   {
-    ImageURL: "/assets/Overview.png",
+    ImageURL: "/assets/Overview.gif",
     Title: "📊 AdventureWorks Project: ETL to Data Warehouse and Sales Dashboard",
     Description: "This project involves extracting data from the AdventureWorks transactional database, transforming it into a structured format, loading it into a Data Warehouse (DW), and creating an interactive Sales Dashboard. The primary goal is to demonstrate skills in ETL development, data modeling, and business intelligence visualization.",
     Technologies: [
@@ -77,7 +77,7 @@ export const projects = [
     Source: "https://github.com/mohamed1kot/Sales_DW", // Optional!
   },
   {
-    ImageURL: "/assets/Emergency-Room-Dashboard.png",
+    ImageURL: "/assets/Emergency-Room-Dashboard.gif",
     Title: "🏥 Emergency Room Dashboard",
     Description: "This project provides a comprehensive data-driven analysis of a modern hospital Emergency Room using an interactive Power BI dashboard. It focuses on data from 2020, offering insights into patient flow, demographics, satisfaction, and operational efficiency.",
     Technologies: [
@@ -93,7 +93,7 @@ export const projects = [
     Source: "https://github.com/mohamed1kot/Emergency-Room-Dashboard", // Optional!
   },
   {
-    ImageURL: "/assets/Airlines_Delayes.png", //
+    ImageURL: "/assets/Airlines_Delayes.gif", //
     Title: "✈️ Airlines Delays 2008 - Data Analysis Project",
     Description:
       "This project provides a comprehensive visual and analytical review of U.S. airline operations and delays throughout the year 2008. Using real-world data and visual storytelling, we dive into the scale, causes, and patterns of delays and cancellations that shaped the passenger experience.",
@@ -109,7 +109,7 @@ export const projects = [
     Source: "https://github.com/mohamed1kot/Airline_Delay_USA_analysis", // Optional!
   },
   {
-    ImageURL: "/assets/Athelete_Details.png",
+    ImageURL: "/assets/Athelete_Details.gif",
     Title: "🏅 Olympic Data Analysis Project",
     Description:
       "This project explores the Olympic Games through rich visualizations and interactive dashboards, offering insights into athletes, countries, sports, and medal achievements. The analysis is performed using Power BI, visualizing a large dataset of Olympic history.",
@@ -125,7 +125,7 @@ export const projects = [
     Source: "https://github.com/mohamed1kot/Athelete_Events_analysis", // Optional!
   },
   {
-    ImageURL: "/assets/Products.png",
+    ImageURL: "/assets/Products.gif",
     Title: "🛒 Olist E-commerce Analysis Dashboard",
     Description: "Welcome to the Olist E-commerce Dashboard project. This Power BI report provides deep, interactive insights into the operational dynamics of the Olist online shopping platform. It spans across multiple business domains including customers, products, sales, sellers, geography, and predictive analytics.",
     Technologies: [
